1844a48cedabb4fb2a17d24c8612562fc26f5a33
Prachi Dange <114732142+PrachiDange0702@users.noreply.github.com>
2023-08-29 16:09:02 +0530

    Create RemoveduplicateElementfromsortedLinkedList.java

---
+++ Day 2/RemoveduplicateElementfromsortedLinkedList.java
@@ -0,0 +1,31 @@
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+classRemoveduplicateElementfromsortedLinkedList
+{
+    //Function to remove duplicates from sorted linked list.
+    Node removeDuplicates(Node head)
+    {
+	// Your code here	
+	
+	
+Node ptr = head;
+	    while(ptr.next != null){
+	        while(ptr.data == ptr.next.data){
+	            ptr.next = ptr.next.next;
+	            if(ptr.next == null){
+	               return head;
+	            }
+	        }
+	        ptr = ptr.next; 
+	    }
+	    return head;
+	
+	
+    }
+}
