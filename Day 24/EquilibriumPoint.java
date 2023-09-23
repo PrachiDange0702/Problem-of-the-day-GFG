ad3e493724011b101c93e824af3ceeb085a1bf53
Prachi Dange <114732142+PrachiDange0702@users.noreply.github.com>
2023-09-23 16:33:44 +0530

    Create EquilibriumPoint.java

---
+++ Day 24/EquilibriumPoint.java
@@ -0,0 +1,19 @@
+long sum = 0; // initialize sum of whole array
+        long leftsum = 0; // initialize leftsum
+ 
+        /* Find sum of the whole array */
+        for (int i = 0; i < n; ++i)
+            sum += arr[i];
+ 
+        for (int i = 0; i < n; ++i) {
+            sum -= arr[i]; // sum is now right sum for index
+                           // i
+ 
+            if (leftsum == sum)
+                return i;
+ 
+            leftsum += arr[i];
+        }
+ 
+        /* If no equilibrium index found, then return 0 */
+        return -1;
