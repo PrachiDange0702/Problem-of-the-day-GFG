65b733a1e50ba4ea669e12a1ad4bd99334605aab
Prachi Dange <114732142+PrachiDange0702@users.noreply.github.com>
2023-10-12 15:45:28 +0530

    Create DuplicatesBinaryTree.java

---
+++ Day 43/DuplicatesBinaryTree.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    HashMap<String ,Integer> hm = new HashMap<>();
+
+    int dupSub(Node root) {
+
+        solve(root);
+
+            for(String a:hm.keySet()){
+
+                 if(hm.get(a)>1)
+
+        return 1;
+
+            }
+
+    return 0;
+
+}
+
+String solve(Node root){
+
+    Node curr = root ;
+
+    if(curr == null)
+
+    return "";
+
+    String ans = "";
+
+    String l = solve(curr.left);
+
+    String r = solve(curr.right);
+
+    ans = l+curr.data+r;
+
+    if(curr.left != null || curr.right!= null)
+
+    hm.put(ans,hm.getOrDefault(ans,0)+1);
+
+    return ans;
+
+}
