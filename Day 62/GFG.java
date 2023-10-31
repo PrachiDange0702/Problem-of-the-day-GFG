55706e9683868ddbbec7f2fb3006183f98236bba
Prachi Dange <114732142+PrachiDange0702@users.noreply.github.com>
2023-10-31 16:53:42 +0530

    Create GFG.java

---
+++ Day 62/GFG.java
@@ -0,0 +1,30 @@
+Problem of the day - Move all zeroes to end of array.
+
+Link of the Question - https://practice.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1
+
+Question - Given an array arr[] of n positive integers. Push all the zeros of the given array to the right end of the array while maintaining the order of non-zero elements. Do the mentioned change in the array in-place.
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {3, 5, 0, 0, 4}
+Output: 3 5 4 0 0
+Explanation: The non-zero elements
+preserve their order while the 0
+elements are moved to the right.
+Example 2:
+
+Input:
+N = 4
+Arr[] = {0, 0, 0, 4}
+Output: 4 0 0 0
+Explanation: 4 is the only non-zero
+element and it gets moved to the left.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 105
+0 ≤ arri ≤ 105
