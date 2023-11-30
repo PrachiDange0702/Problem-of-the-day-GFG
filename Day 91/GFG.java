120ee22202375fc5821920acda0ca226e021254a
Prachi Dange <114732142+PrachiDange0702@users.noreply.github.com>
2023-11-30 21:38:47 +0530

    Create GFG.java

---
+++ Day 91/GFG.java
@@ -0,0 +1,12 @@
+ int steps = 0;
+
+        while (n > 1) {
+            if (n % 3 == 0) {
+                n /= 3;
+            } else {
+                n -= 1;
+            }
+            steps++;
+        }
+
+        return steps;
